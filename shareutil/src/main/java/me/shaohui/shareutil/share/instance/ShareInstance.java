0295d9dc1fdadaee14047e7368dd9fa2ce57f261
LEO <973555610@qq.com>
2018-04-29 11:10:53 +0800

    添加分享说明

--- shareutil/src/main/java/me/shaohui/shareutil/share/instance/ShareInstance.java
+++ shareutil/src/main/java/me/shaohui/shareutil/share/instance/ShareInstance.java
@@ -18,6 +18,19 @@ public interface ShareInstance {
     void shareMedia(int platform, String title, String targetUrl, String summary, final String miniId, final String miniPath,
                     ShareImageObject shareImageObject, Activity activity, ShareListener listener);
 
+    /**
+     * 分享图文
+     * @param platform           分享类型
+     * @param title              分享标题
+     * @param targetUrl          分享后跳转链接
+     * @param summary            分享说明
+     * @param miniId             小程序id
+     * @param miniPath           小程序path
+     * @param shareImageObject   分享图标对象
+     * @param shareImmediate     是否直接分享（不对图片二次处理）
+     * @param activity           activity
+     * @param listener           分享结果
+     */
     void shareMedia(int platform, String title, String targetUrl, String summary, final String miniId, final String miniPath,
                     ShareImageObject shareImageObject, boolean shareImmediate, Activity activity, ShareListener listener);
 
